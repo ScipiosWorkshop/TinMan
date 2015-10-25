49d15ecf7d8ce0ba949761e5313d5775477c068d
Robotnik <evanusa@cs.umd.edu>
2015-10-24 21:33:11 -0400

    Started header file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-To Be Filled in
+To be completed: this is going to be a comprehensive calculator for magic card probabilities.
---
+++ probability.h
@@ -0,0 +1,15 @@
+#ifndef PROBABILITY_H
+#define PROBABILITY_H
+
+#include <stdlib.h>
+
+
+
+//Function Declarations
+int n_choose_k(int n, int k);
+int factorial(int number);
+int calculate_hypergeometric(int successes, int draws, int population_size, int total_successes);
+
+
+
+#endif
